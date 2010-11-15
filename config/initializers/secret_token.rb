e4c134c927b3ecac204cba3a94145fadee79e648
wfjackson3 <willis@mashtunbeer.com>
2010-11-14 21:42:21 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GpuDosing::Application.config.secret_token = 'deb3cd2c91f5fba7a4207fc11c2d9d4dbadc6cde03c02b2424397207c93f91c959700c961d7c22c9725f51dd46670de787a6b6cd0b63416e0e9faa88646723e1'
