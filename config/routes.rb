3d011a5ea491ceb7946b412624744d13a8468eb5
wfjackson3 <willis@mashtunbeer.com>
2010-11-21 16:26:34 -0600

    finished very rough version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 GpuDosing::Application.routes.draw do
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ GpuDosing::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
