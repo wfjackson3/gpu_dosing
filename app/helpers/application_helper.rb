323347a60ec6abc59a8e5cfe313dbb5e2ad59db3
wfjackson3 <willis@mashtunbeer.com>
2010-11-21 16:37:23 -0600

    fixed the titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # return a title on a per page basis
+  def title
+    base_title="Grundfos Pumps Dosing Parts Selector"
+    if @title.nil?
+      base_title
+    else
+      '#{base_title} | {@title}'
+    end
+  end
 end
