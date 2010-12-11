1edaa6d56cdbed97456a2bba50aaf01a68d159ac
wfjackson3 <willis@mashtunbeer.com>
2010-12-11 14:32:41 -0600

    Planning what is next

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # return a title on a per page basis
   def title
-    base_title="Grundfos Pumps Dosing Parts Selector"
+    base_title = "Grundfos Pumps Dosing Parts Selector"
     if @title.nil?
       base_title
     else
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,5 @@
 module PagesHelper
+  # Fill this in so it returns the kit object if only 1 kit remains from the filter
+  # Handle returning text explanations if the results are 0 or many kits
+  # Should I add conditional statements to the find view to properly render the return of this helper?
 end
