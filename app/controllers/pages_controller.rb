a75551e4f51d2e9a4476e13f52b124f5e8380963
wfjackson3 <willis@mashtunbeer.com>
2010-12-14 18:27:10 -0600

    Holy crap stuff is starting to work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,6 +23,13 @@ class PagesController < ApplicationController
       @valve = @valve.where(:line_id => params[:line][:line_id]) if !params[:line][:line_id].blank?
       @valve = @valve.where(:head_id => params[:head][:head_id]) if !params[:head][:head_id].blank?
       @valve = @valve.where(:gasket_id => params[:gasket][:gasket_id]) if !params[:gasket][:gasket_id].blank?
+      @valve = @valve.where(:dnsize_id => Size.find(:first, :conditions => {:id => params[:size][:size_id]}).dnsize_id)
+      # This actually works
+      # size = Size.find(:first, :conditions => {:id => params[:size][:size_id]} ).dnsize_id
+      
+      # Returns a relation, but does not work with .dnsize_id appended
+      # @size = Size.where(:id => params[:size][:size_id]).dnsize_id
+      
       # repeat for each piece of information required to uniquely identify a given kit
       @numb = @valve.length
     end
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,2 +1,9 @@
 class Size < ActiveRecord::Base
+  
+  belongs_to :line
+  belongs_to :dnsize
+  
+  def flow_pressure
+    "#{flow} - #{pressure}"
+  end
 end
